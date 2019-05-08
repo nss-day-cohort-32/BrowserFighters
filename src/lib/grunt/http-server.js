fa52f608cef3a2cb5811b82490159c2bb661e00d
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-05-08 15:08:22 -0500

    adjusted file paths

---
+++ src/lib/grunt/http-server.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dev: {
+    root: "./../../public/",
+    port: 8080,
+    showDir: false,
+    autoIndex: true,
+    ext: "html",
+    runInBackground: true,
+    openBrowser: false
+  }
+};
