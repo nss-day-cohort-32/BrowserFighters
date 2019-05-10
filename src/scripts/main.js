5d9d51edc25fa41f118ec2ef7698939e67b6b50f
Matthew Abebe <matthewabebe.inbox@gmail.com>
2019-05-10 15:06:17 -0500

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,14 @@ import {test} from "./modules/test";
 import {API} from "./modules/apiCalls";
 import {login} from "./modules/loginPage";
 import {regBtn} from "./modules/registerUser";
+import {friendSearch} from "./modules/friends";
+import {searchAddFriend} from "./modules/friends";
 
 regBtn;
 login;
 test();
+friendSearch();
+searchAddFriend;
 
 API.getUsers().then(entries => console.log("allUsers", entries));
 API.getOneUser(1).then(entry => console.log("one user", entry));
---
+++ src/scripts/modules/friends.js
@@ -0,0 +1,47 @@
+import { API } from "./apiCalls"
+
+const friendsEl = document.getElementById("friendsContainer");
+
+export function friendSearch() {
+    const friendsButton = document.getElementById("addFriendsBtn").addEventListener("click", function (event) {
+
+        const searchFriends = document.createElement("input");
+        searchFriends.setAttribute("class", "searchFriend");
+        console.log("searchFriends", searchFriends)
+        searchFriends.value = "Search Friends";
+        document.body.appendChild(searchFriends);
+
+        const searchFriendsButton = document.createElement("button")
+
+        searchFriendsButton.setAttribute("class", "searchFriendsButton");
+        searchFriendsButton.textContent = "Search";
+        document.body.appendChild(searchFriendsButton);
+
+        searchFriendsButton.addEventListener("click", function (event) {
+
+            API.getUsers().then(entries => {
+
+                let findFriend
+                for (let i = 0; i < entries.length; i++) {
+                    const friend = entries[i]
+                    if (searchFriends.value === friend.user_name) {
+                        console.log("found matching name")
+                        findFriend = friend
+                        break }
+                    else {
+                        console.log("user not found");
+                    }
+                }
+                console.log(findFriend)
+                let findfriendHtml =`<h2>${findFriend.user_name}</h2>
+                <button>Add Friend</button>`
+                console.log(findfriendHtml)
+
+                friendsEl.innerHTML += findfriendHtml
+
+            })
+        })
+    })
+}
+
+
